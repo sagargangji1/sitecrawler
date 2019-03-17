5dcda3b79e9cf2e95a4b5c45051952d46a1cbdb2
sagar <sagargangji1@gmail.com>
2019-03-17 22:37:27 +0530

    Application for Crawling Pages

---
+++ crawler/src/main/java/com/crawler/service/CrawlService.java
@@ -0,0 +1,27 @@
+package com.crawler.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.crawler.model.SiteMap;
+import com.crawler.utils.CrawlSite;
+/**
+ * 
+ * @author Sagar Gangji
+ * sagargangji1@gmail.com
+ */
+
+
+@Component
+public class CrawlService {
+
+	public SiteMap crawlSite(String url , List<String> domain ,int noOfPagesToCrawl){
+		Map<String, SiteMap> visitedUrl = new HashMap<>();
+		CrawlSite crawlSite = new CrawlSite(true ,domain , noOfPagesToCrawl);
+		return  crawlSite.crawlUrl(url, null,  visitedUrl );
+	}
+	
+}
